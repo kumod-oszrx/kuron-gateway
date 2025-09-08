c361c61b2efd90d2af28fee5604638ee336e73b8
datnt19213 <datnguyen.frontend.dev@gmail.com>
2025-09-09 00:03:26 +0700

    ✨ feat(route): implement rest and graphql proxy routes
    - add restRouter to proxy rest api requests
    - add graphqlRouter to proxy graphql api requests
    - remove duplicate axios config

---
+++ src/routes/graphql/index.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+import { Enum } from '../../configs/enum';
+
+export const graphqlRouter = async (req: any, res: any) => {
+  try {
+    // console.log(req.path); // log is /users for call localhost:5000/api/g/users
+    const url = `${process.env.PAYLOAD_URL || Enum.PAYLOAD_URL}/api/graphql${
+      req.path
+    }`;
+    const response = await axios.request({
+      method: req.method,
+      url,
+      headers: req.headers,
+      data: req.body,
+    });
+    res.json({ ...response.data });
+  } catch (error) {
+    res.status(500).json({ message: "Error processing request" });
+  }
+};
--- src/routes/init.ts
+++ src/routes/init.ts
@@ -1,44 +1,23 @@
-import axios from 'axios';
 import { Router } from 'express';
 
-import { Enum } from '../configs/enum';
 import authRouter from './auth';
+import { graphqlRouter } from './graphql';
+import { restRouter } from './rest';
 
 const routers = Router();
 
-routers.use("/r", async (req, res) => {
-    // console.log(req.path); // log is /users for call localhost:5000/api/r/users
-    const url = `${process.env.PAYLOAD_URL || Enum.PAYLOAD_URL}/api${req.path}`;
-    const response = await axios.request({
-        method: req.method,
-        url,
-        headers: req.headers,
-        data: req.body,
-    })
-    res.send(response.data);
-});
-routers.use("/g", async (req, res) => {
-    console.log(req.path);
-    const url = `${process.env.PAYLOAD_URL || Enum.PAYLOAD_URL}/api/graphql${req.path}`;
-    const response = await axios.request({
-        method: req.method,
-        url,
-        headers: req.headers,
-        data: req.body,
-    })
-    res.send(response.data);
-    // res.send(req.path)
-});
+routers.use("/r", restRouter);
+routers.use("/g", graphqlRouter);
 routers.use("/auth", authRouter);
 
-routers.get('/debug', async (req, res) => {
-  try {
-    const r = await axios.get(`${process.env.PAYLOAD_URL}/api/users`, { timeout: 10000 });
-    res.json({ ok: true, status: r.status });
-  } catch (e:any) {
-    res.status(500).json({ ok:false, message: e.message, code: e.code, stack: e.stack });
-  }
-});
+// routers.get('/debug', async (req, res) => {
+//   try {
+//     const r = await axios.get(`${process.env.PAYLOAD_URL}/api/users`, { timeout: 10000 });
+//     res.json({ ok: true, status: r.status });
+//   } catch (e:any) {
+//     res.status(500).json({ ok:false, message: e.message, code: e.code, stack: e.stack });
+//   }
+// });
 
 
 export default routers;
