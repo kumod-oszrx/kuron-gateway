b6b71651ae2c25ad33a64a43b66c2f9a869677e8
datnt19213 <datnguyen.frontend.dev@gmail.com>
2025-09-08 23:25:01 +0700

    ♻️ refactor(routes): remove proxy middleware and use axios directly
    - remove graphqlRouter and restRouter
    - use axios to forward requests to PayloadCMS REST API and GraphQL API
    - remove http-proxy-middleware

--- src/routes/init.ts
+++ src/routes/init.ts
@@ -1,14 +1,35 @@
+import axios from 'axios';
 import { Router } from 'express';
 
+import { Enum } from '../configs/enum';
 import authRouter from './auth';
 import router from './auth';
-import graphqlRouter from './graphql';
-import restRouter from './rest';
 
 const routers = Router();
 
-routers.use("/r", restRouter);
-routers.use("/g", graphqlRouter);
+routers.use("/r", async (req, res) => {
+    // console.log(req.path); // log is /users for call localhost:5000/api/r/users
+    const url = `${process.env.PAYLOAD_URL || Enum.PAYLOAD_URL}/api${req.path}`;
+    const response = await axios.request({
+        method: req.method,
+        url,
+        headers: req.headers,
+        data: req.body,
+    })
+    res.send(response.data);
+});
+routers.use("/g", async (req, res) => {
+    console.log(req.path);
+    const url = `${process.env.PAYLOAD_URL || Enum.PAYLOAD_URL}/api/graphql${req.path}`;
+    const response = await axios.request({
+        method: req.method,
+        url,
+        headers: req.headers,
+        data: req.body,
+    })
+    res.send(response.data);
+    // res.send(req.path)
+});
 router.use("/auth", authRouter);
 
 export default routers;
